22b48afb4744f70f0aa09b65a89ac9c594d3473c
Dharun07-17 <dharunmurugan1@gmail.com>
2026-06-23 16:37:24 +0400

    made the ui transparent

--- electron/main.js
+++ electron/main.js
@@ -1,27 +1,22 @@
-import {BrowserWindow, app} from 'electron';
-import path from 'path';
-import {fileURLToPath} from 'url';
+import { BrowserWindow, app,Menu } from "electron";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 700,
+    height: 600,
 
-function createwindow() {
-    const win = new BrowserWindow({
-        width: 600,
-        height: 600,
+    transparent: true,
+   
 
-         transparent: true,
-  frame: false,
+   
+  });
 
-  vibrancy: "fullscreen-ui",
+  Menu.setApplicationMenu(null);
 
-       
+  win.setBackgroundMaterial("acrylic");
 
-
-    });
-
-  win.loadURL('http://localhost:5173');
+  win.loadURL("http://localhost:5173");
 }
 
-app.whenReady().then(createwindow);
+app.whenReady().then(createWindow);
 
--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Panel from './components/Panel'
 
 const App = () => {
   return (
-    <div className=" bg-black/20 backdrop-blur-xl " >
+    <div className="min-h-screen bg-transparent" >
     
      <Panel />
   
